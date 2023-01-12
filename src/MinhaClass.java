ff054b18f3003e594c4aed2836c77316c154980e
erikasantanaa <erikamariasantanaa@gmail.com>
2023-01-12 02:20:30 -0300

    Primeiro commit

---
+++ src/MinhaClass.java
@@ -0,0 +1,16 @@
+public class MinhaClass {
+    public static void main(String[] args) {
+
+       String primeiroNome = "Érika";
+       String segundoNome = "Santana";
+
+       String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+       System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método " + primeiroNome.concat("").concat(segundoNome);
+    }
+}
