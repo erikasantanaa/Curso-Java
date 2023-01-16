6887f6fdb01f1f5357cd701c0e6548b465c7a6cd
erikasantanaa <erikamariasantanaa@gmail.com>
2023-01-15 22:35:34 -0300

    concluindo aula sobre comentários e doc

---
+++ src/edu/erika/calculadora/Calculadora.java
@@ -0,0 +1,28 @@
+package edu.erika.calculadora;
+ /** <h1>Calculadora</h1>
+ A calculadora realiza operações matemáticas entre
+ números inteiros
+ <p>
+ <b>Note:</p> Leia atentamente a documentação desta 
+ classes para desfrutar dos recursos oferecidos pelo altor
+
+ @author Gleyson Sampaio
+ @version 1.0
+ @since 01/01/2022 
+ */
+
+public class Calculadora() {
+    /**
+     *Este método é utilizado para somar dois números inteiros
+     @param numeroUm este é o segundo parâmetro do método
+     @param numeroDois este é o segundo parâmetro do método
+     @return int o resultado deste método é a soma dos dois números. 
+    */
+
+    public static void main(String[] args) {
+        
+    }
+    public int somar(int numeroUm, int numeroDois) {
+       return numeroUm + numeroDois; 
+    }
+}
