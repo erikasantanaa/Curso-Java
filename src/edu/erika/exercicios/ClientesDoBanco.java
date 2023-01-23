c2f7d25d6a02520e4c5d0d01a9c11a9f4d452eea
erikasantanaa <erikamariasantanaa@gmail.com>
2023-01-22 22:20:23 -0300

    Exercicios de fixação de conteudo Java

--- src/edu/erika/exercicios/ClientesDoBanco.java
+++ src/edu/erika/exercicios/ClientesDoBanco.java
@@ -1,16 +1,24 @@
 package edu.erika.exercicios;
+import java.util.Scanner;
 
 /**
  * Elabore um programa que simule uma fila de atendimento bancário.
  * O programa deve ler o nome de 3 pessoas (clientes do banco), 
  * armazenando-os em uma fila.
+ * 
+ * -> Nome da pessoa.
+ * -> Posição da pessoa na fila.
 */
 
 public class ClientesDoBanco {
     public static void main(String[] args) {
+
          
         String[] nomesFila = new String[3]; 
-        Scanner nome = new Scanner(System.in); 
+        Scanner nome = new Scanner(System.in);
+
+
+        System.out.println();
 
  //Implemente uma condição que simule uma fila bancaria, gerando o nome a posição do cliente na fila:
         
---
+++ src/edu/erika/exercicios/PercentualDeDesconto.java
@@ -0,0 +1,20 @@
+package edu.erika.exercicios;
+import java.util.*;
+ 
+
+/**
+ * O gerente de uma loja resolveu aplicar descontos em 
+ * todos os seus produtos! A tarefa é calcular a 
+ * Porcentagem de Desconto aplicada a esses produtos.
+*/
+
+public class PercentualDeDesconto {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+		
+        int M = input.nextInt();
+        int S = input.nextInt();  
+        
+    //TODO:  Retorne o percentual de desconto que foi aplicado no produto
+    }
+}
